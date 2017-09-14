4872377d312153816bb0004e8cff6007751aa9a7
柴云枫 <dmate@foxmail.com>
2017-09-14 20:35:00 +0800

    beautify code

--- src/G.js
+++ src/G.js
@@ -9,14 +9,14 @@ export default {
   requestMovies (url, that, params = '?count=20') {
     let self = that
     if (self.$store.state[url].length <= 0) {
-      axios.get('/api/movie/' + url + params).then(function (response) {
+      axios.get(`/api/movie/${url + params}`).then(function (response) {
         self.loading = false
         self.items = response.data.subjects
         self.total = response.data.total
         self.$store.dispatch('setMovieItems', {tab: url, value: self.items})
         self.$store.dispatch('setTotal', {tab: url, value: self.total})
       }).catch(function (error) {
-        throw new Error('加载失败 ' + error)
+        throw new Error(`加载错误: ${error}`)
       })
     } else {
       self.loading = false
@@ -26,18 +26,18 @@ export default {
   },
   loadMoreMovies (url, start, that) {
     start = parseInt(start)
-    axios.get('/api/movie/' + url + '?start=' + start + '&count=12').then(function (response) {
+    axios.get(`/api/movie/${url}?start=${start}&count=12`).then(function (response) {
       for (let item of response.data.subjects) {
         that.items.push(item)
       }
       that.$store.dispatch('setMovieItems', {tab: url, value: that.items})
       that.moreLoading = false
     }).catch(function (error) {
-      throw new Error('加载错误' + error)
+      throw new Error(`加载错误: ${error}`)
     })
   },
   requestSingleItem (url) {
-    return axios.get('/api/movie/' + url)
+    return axios.get(`/api/movie/${url}`)
   },
   searchMovie (param) {
     return axios.get(`/api/movie/search?q=${param}`)
