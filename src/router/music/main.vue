ae197b14a5287b5603f9a3794ec2d2fbce1ccf6d
izuomeng <dmate@foxmail.com>
2017-09-17 18:37:49 +0800

    decorate music page

--- src/router/music/main.vue
+++ src/router/music/main.vue
@@ -1,6 +1,9 @@
+<style lang="less" src="./style.less"></style>
+
 <template>
-  <div>
-    Music page
+  <div class="music">
+    <img src="../../assets/music.png" width="300px"/><br>
+    欢迎来到音乐页！在右上方搜索栏查找你想听的音乐
   </div>
 </template>
 
