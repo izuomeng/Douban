6ef397263632fa81b7e3726e59ccee38b7774d58
izuomeng <dmate@foxmail.com>
2017-09-17 20:12:46 +0800

    finish

--- src/router/about/main.vue
+++ src/router/about/main.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="about-me">
     <img src="../../assets/me.png" width="300px"/><br>
-    这是用vue做的第一个小型项目, 感谢豆瓣提供的API，由于没有申请开发者账号，因此读书和音乐模块暂时无法使用
+    这是用vue做的第一个小型项目，感谢豆瓣提供的API，由于没有申请开发者账号，因此读书和音乐模块暂时无法使用
   </div>
 </template>
 
